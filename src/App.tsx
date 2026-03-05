fd452d4a23cc80a2f39a9e640c88e03c685187c1
Mikhail Uljakov <mikhail.ulyakov@icloud.com>
2026-03-06 01:14:45 +0700

    CALL-17 prettier fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { StrictMode } from 'react'
+import { StrictMode } from "react"
 
-import { ThemeProvider } from '@/components/theme-provider'
+import { ThemeProvider } from "@/components/theme-provider"
 
-import { ThemeToggler } from '@/components/ThemeToggler'
+import { ThemeToggler } from "@/components/ThemeToggler"
 
 function App() {
-    return (
-        <StrictMode>
-            <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-                <ThemeToggler />
-            </ThemeProvider>
-        </StrictMode>
-    )
+  return (
+    <StrictMode>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <ThemeToggler />
+      </ThemeProvider>
+    </StrictMode>
+  )
 }
 
 export default App
