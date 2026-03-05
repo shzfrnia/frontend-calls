2bd1274b173cacd06c8c11c5e12397cc21c1c502
Mikhail Uljakov <33577889+shzfrnia@users.noreply.github.com>
2026-03-06 02:26:57 +0700

    CALL-19 init localization lib (#4)
    Co-authored-by: Mikhail Uljakov <mikhail.ulyakov@icloud.com>

---
+++ src/components/LocalizationToggler/index.tsx
@@ -0,0 +1,35 @@
+import { useTranslation } from "react-i18next";
+import { Moon, Sun } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export function LocalizationToggler() {
+  const { i18n } = useTranslation();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Button variant="outline">{i18n.language}</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {(["ru", "en"] as const).map((themeName) => {
+          return (
+            <DropdownMenuItem
+              key={themeName}
+              disabled={i18n.language === themeName}
+              onClick={() => i18n.changeLanguage(themeName)}
+            >
+              {themeName}
+            </DropdownMenuItem>
+          );
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
