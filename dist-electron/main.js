ac6c2d1c6cd09abc5e9f1470422e2460add427d9
Mikhail Uljakov <mikhail.ulyakov@icloud.com>
2026-03-14 05:46:50 +0700

    CALL-34 add min viewport

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -12,6 +12,8 @@ process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, 
 let win;
 function createWindow() {
   win = new BrowserWindow({
+    minWidth: 800,
+    minHeight: 500,
     icon: path.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
     webPreferences: {
       preload: path.join(__dirname$1, "preload.mjs")
