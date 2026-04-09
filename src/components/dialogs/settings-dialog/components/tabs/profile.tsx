c98c284c29270ad79281fc916822284218836b47
Mikhail Uljakov <mikhail.ulyakov@icloud.com>
2026-04-10 06:47:03 +0700

    CALL-60 deveop settings

--- src/components/dialogs/settings-dialog/components/tabs/profile.tsx
+++ src/components/dialogs/settings-dialog/components/tabs/profile.tsx
@@ -1,3 +1,20 @@
+import { useAppSelector } from "@/hooks/use-store"
+
+import { selectCurrentUser } from "@/store/slices/auth-slice"
+
 export function SettingsProfile() {
-  return <div>SettingsProfile</div>
+  const currentUser = useAppSelector(selectCurrentUser)
+
+  return (
+    <div className="flex flex-col gap-3">
+      {Object.entries(currentUser || {}).map(([field, value]) => {
+        return (
+          <div key={field} className="flex gap-2">
+            <p>{field}:</p>
+            <p>{value}</p>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
