40de7ca107ed5336d17bac8a8c87796c860f3917
Mikhail Uljakov <mikhail.ulyakov@icloud.com>
2026-04-11 01:12:40 +0700

    CALL-60 WIP

--- src/components/dialogs/settings-dialog/components/main-block.tsx
+++ src/components/dialogs/settings-dialog/components/main-block.tsx
@@ -5,13 +5,15 @@ export function MainBlock({
   header,
 }: {
   children: ReactNode
-  header: string
+  header?: string
 }) {
   return (
     <div>
-      <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight mb-3">
-        {header}
-      </h3>
+      {header && (
+        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight mb-3">
+          {header}
+        </h3>
+      )}
       <div className="flex flex-col gap-5 items-baseline">{children}</div>
     </div>
   )
--- src/components/dialogs/settings-dialog/components/tabs/application.tsx
+++ src/components/dialogs/settings-dialog/components/tabs/application.tsx
@@ -1,11 +1,37 @@
+import { useTranslation } from "react-i18next"
+
 import { ThemeToggler } from "@/components/theme-toggler"
 import { LocalizationToggler } from "@/components/localization-toggler"
 
+import { MainBlock } from "../main-block"
+import { SecondaryBlock } from "../secondary-block"
+
 export function SettingsApplication() {
+  const { t } = useTranslation()
+
   return (
-    <div className="flex flex-col gap-3">
-      <ThemeToggler />
-      <LocalizationToggler />
-    </div>
+    <MainBlock>
+      <SecondaryBlock
+        header={t(
+          "dialogs.settings.nav.general.nav.application-settings.choose-theme.header"
+        )}
+        description={t(
+          "dialogs.settings.nav.general.nav.application-settings.choose-theme.description"
+        )}
+      >
+        <ThemeToggler />
+      </SecondaryBlock>
+
+      <SecondaryBlock
+        header={t(
+          "dialogs.settings.nav.general.nav.application-settings.choose-language.header"
+        )}
+        description={t(
+          "dialogs.settings.nav.general.nav.application-settings.choose-language.description"
+        )}
+      >
+        <LocalizationToggler />
+      </SecondaryBlock>
+    </MainBlock>
   )
 }
