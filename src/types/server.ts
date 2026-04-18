88c5c67e7da0534f6537c5bf14c67966e1890149
Mikhail Uljakov <mikhail.ulyakov@icloud.com>
2026-04-18 07:23:43 +0700

    CALL-68 WIP

--- src/types/server.ts
+++ src/types/server.ts
@@ -1,13 +1,28 @@
 import type { uuid4 } from "."
 
-export type Channel = {
-  id: uuid4
-  name: string
-}
-
 export type Server = {
   id: uuid4
   name: string
 }
 
 export type ServerDraft = Omit<Server, "id">
+
+export type Channel = {
+  id: uuid4
+  name: string
+  order: number
+  category_id: uuid4
+  server_id: uuid4
+}
+
+export type VoiceChannel = Channel & {
+  settings: { limit: number }
+}
+
+export type Category = {
+  id: uuid4
+  order: number
+  name: string
+  server_id: uuid4
+  channels: Channel[]
+}
